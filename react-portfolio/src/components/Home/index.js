326ebd066296d1a57a53f6804f9d10abf8e1e4ac
Bipin_Kalakheti <100770518+Bipin-Kalakheti@users.noreply.github.com>
2024-04-26 18:50:27 -0400

    add svg logo animation

--- react-portfolio/src/components/Home/index.js
+++ react-portfolio/src/components/Home/index.js
@@ -1,26 +1,72 @@
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import LogoTitle from '../../assets/images/logo-B.png'
-import './index.scss';
-
-
+import './index.scss'
+import { useEffect, useState } from 'react'
+import AnimatedLetters from '../AnimatedLetters'
+import Logo from './Logo'
 
 const Home = () => {
-    return (
-        <div className="container home-page">
-            <div className="text-zone">
-                <h1>Hi, <br /> I'm 
-                <img src={LogoTitle} alt="developer" />
-                ipin
-                <br />
-                web developer
-                </h1>
-                <h2> Frontend Developer / Python Developer / Gamer</h2>
-                <Link to="/contact" className="flat-button">CONTACT ME</Link>
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = ['i', 'p', 'i', 'n']
+  const jobArray = [
+    'w',
+    'e',
+    'b',
+    ' ',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ]
 
-            </div>
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+
+    // Return a cleanup function to clear the timeout if the component unmounts
+    return () => clearTimeout(timer)
+  }, [])
+
+  return (
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i</span>
+            <span className={`${letterClass} _13`}>,</span>
+            <br />
+            <span className={`${letterClass} _14`}>I</span>
+            <span className={`${letterClass} _15`}>'m</span>
+            <img src={LogoTitle} alt="developer" />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={22}
+            />
+          </h1>
+          <h2> Frontend Developer / Python Developer / Gamer</h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
         </div>
-    )
+        <Logo />
+      </div>
+    </>
+  )
 }
-
 
 export default Home
