7acf570ec0ecd401f815fefbbc76c631761fb98a
Bipin_Kalakheti <100770518+Bipin-Kalakheti@users.noreply.github.com>
2024-04-27 20:45:38 -0400

    implement contact form

---
+++ react-portfolio/src/components/Contact/index.js
@@ -0,0 +1,93 @@
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
+import { useEffect, useRef, useState } from 'react'
+import emailjs from '@emailjs/browser'
+
+const Contact = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const refForm = useRef()
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+
+    // Return a cleanup function to clear the timeout if the component unmounts
+    return () => clearTimeout(timer)
+  }, [])
+
+
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+    emailjs.sendForm(
+        'gmail',
+        'ajkadkasj',
+        refForm.current,
+        'dajfakfakfjalf'
+    )
+    .then (
+    () => {
+        alert('Message sent!')
+        window.location.reload(false)
+    },
+    () => {
+        alert('Oops! There was an error.')
+    }
+    )
+  }
+
+  return (
+    <>
+      <div className="contact-page container">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I am interested in programming languages and programming languages.
+          </p>
+          <div className="contact-form">
+            <form ref={refForm} onSubmit={sendEmail}>
+              <ul>
+                <li className="half">
+                  <input type="text" name="name" placeholder="Name" required />
+                </li>
+                <li className="half">
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    placeholder="Subject"
+                    type="text"
+                    name="subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea placeholder="Message" name="message" required />
+                </li>
+                <li>
+                  <input type="submit" className="flat-button" value="SEND" />
+                </li>
+              </ul>
+            </form>
+          </div>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default Contact
