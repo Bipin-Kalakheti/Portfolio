71b1f30efc1c3ce237f9a1030615d9d4b56bc44d
Bipin_Kalakheti <100770518+Bipin-Kalakheti@users.noreply.github.com>
2024-05-22 22:34:50 -0400

    implemented hamburger navbar

--- react-portfolio/src/components/Sidebar/index.js
+++ react-portfolio/src/components/Sidebar/index.js
@@ -4,6 +4,8 @@ import Logos from '../../assets/images/logo-linesB.png'
 import LogoSubtitle from '../../assets/images/logo_subB.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
+  faBars,
+  faClose,
   faEnvelope,
   faHome,
   faSuitcase,
@@ -15,72 +17,91 @@ import {
   faYoutube,
   faSkype,
 } from '@fortawesome/free-brands-svg-icons'
+import { useState } from 'react'
 
-const Sidebar = () => (
-  <>
-    <div className="nav-bar">
-      <Link className="logo" to="/">
-        <img src={Logos} alt="logo" />
-        <img className="sub-logo" src={LogoSubtitle} alt="Bipin" />
-      </Link>
+const Sidebar = () => {
+  const [showNav, setShowNav] = useState(false)
 
-      <nav>
-        <NavLink
-          exact="true"
-          activeclassname="active"
-          className="home-link"
-          to="/"
-        >
-          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
-        </NavLink>
-        <NavLink
-          exact="true"
-          activeclassname="active"
-          className="about-link"
-          to="/about"
-        >
-          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
-        </NavLink>
-        <NavLink
-          exact="true"
-          activeclassname="active"
-          className="portfolio-link"
-          to="/portfolio"
-        >
-          <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
-        </NavLink>
-        <NavLink
-          exact="true"
-          activeclassname="active"
-          className="contact-link"
-          to="/contact"
-        >
-          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
-        </NavLink>
-      </nav>
+  return (
+    <>
+      <div className="nav-bar">
+        <Link className="logo" to="/">
+          <img src={Logos} alt="logo" />
+          <img className="sub-logo" src={LogoSubtitle} alt="Bipin" />
+        </Link>
 
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href="https://www.linkedin.com/in/bipin-kalakheti/"
+        <nav className={showNav ? 'mobile-show' : ''}>
+          <NavLink
+            exact="true"
+            activeclassname="active"
+            className="home-link"
+            to="/"
           >
-            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href="https://github.com/Bipin-Kalakheti"
+            <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+          </NavLink>
+          <NavLink
+            exact="true"
+            activeclassname="active"
+            className="about-link"
+            to="/about"
           >
-            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
-          </a>
-        </li>
-      </ul>
-    </div>
-  </>
-)
+            <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+          </NavLink>
+          <NavLink
+            exact="true"
+            activeclassname="active"
+            className="portfolio-link"
+            to="/portfolio"
+          >
+            <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
+          </NavLink>
+          <NavLink
+            exact="true"
+            activeclassname="active"
+            className="contact-link"
+            to="/contact"
+          >
+            <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+          </NavLink>
+          <FontAwesomeIcon
+            icon={faClose}
+            color="#ffd700"
+            size="3x"
+            className="close-icon"
+            onClick={() => setShowNav(false)}
+          />
+        </nav>
+
+        <ul>
+          <li>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.linkedin.com/in/bipin-kalakheti/"
+            >
+              <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+            </a>
+          </li>
+          <li>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/Bipin-Kalakheti"
+            >
+              <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+            </a>
+          </li>
+        </ul>
+        <FontAwesomeIcon
+          onClick={() => setShowNav(true)}
+          icon={faBars}
+          color="#ffd700"
+          size="3x"
+          className="hamburger-icon"
+        />
+      </div>
+    </>
+  )
+}
 
 export default Sidebar
