299df961cd79b8b5a5e0c46f597e6fb3279e82b0
Bipin Kalakheti <100770518+Bipin-Kalakheti@users.noreply.github.com>
2024-12-06 10:32:23 -0500

    chore: Update index for AnimatedLetters component in Home for improved animation timing

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -134,7 +134,7 @@ const Home = () => {
             <AnimatedLetters
               letterClass={letterClass}
               strArray={jobArray}
-              idx={22}
+              idx={18}
             />
           </h1>
           <h2>
